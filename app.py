41f76265d565966113f5f3b1ccf358f1e2e79cf9
SanayNagwani <nagwanisanay@gmail.com>
2026-07-31 16:21:18 +0530

    Fix typo in variable name for prompt generation

--- app.py
+++ app.py
@@ -158,10 +158,10 @@ if all(ALL_API) and user_query:
     st.header("Create PPT")
     if st.button("Click to generate: ", key = "generate_ppt_button"):
       with st.spinner("Running agent.."):
-        final_propmt = prompt_generator(model,user_query)
+        final_prompt = prompt_generator(model,user_query)
   
         response = agent.invoke({'messages':[{'role':"user",
-                                              "content":final_prompt}]})
+                                            "content":final_prompt}]})
         
         code = response['messages'][-1].content[-1]['text']
         st.html(code, width="stretch",
